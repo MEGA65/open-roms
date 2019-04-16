dab82edfc8db4f9d50d154d599aadebc412c1e7c
Paul Gardner-Stephen <paul@servalproject.org>
2019-04-17 05:03:13 +0930

    add more routine descriptions, begin documenting why each is needed

---
+++ c64/kernal/ffea.s
@@ -0,0 +1,4 @@
+; $FFEA - Update jiffy clock vector
+; (http://codebase64.org/doku.php?id=base:kernalreference)
+
+	.word update_jiffy_clock
---
+++ c64/kernal/ffed.s
@@ -0,0 +1,4 @@
+; $FFED - Return screen size (X=columns, Y=rows)
+; (http://codebase64.org/doku.php?id=base:kernalreference)
+
+	.word get_screen_size

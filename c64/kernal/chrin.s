5799c5e5e606d45a628ea854405bc85b93ebb98c
Paul Gardner-Stephen <paul@servalproject.org>
2019-04-19 12:45:08 +0930

    add some further information about the CHRIN routine

--- c64/kernal/chrin.s
+++ c64/kernal/chrin.s
@@ -1,3 +1,9 @@
-; Function defined on pp272-273 of C64 Programmers Reference Guide
+				; Function defined on pp272-273 of C64 Programmers Reference Guide
+	;; Compute's Mapping the 64, p228
+	;;  Reads a byte of input, unless from keyboard.
+	;; If from keyboard, then it gets a whole line of input, and returns the first char.
+	;; Repeated calls after that read out the successive bytes of the line of input.
 chrin:
+	lda #$00
+	clc
 	rts

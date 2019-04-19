a5183785ea8d8f329c2cb3f4b1dcc14308a34dc9
Paul Gardner-Stephen <paul@servalproject.org>
2019-04-19 23:00:59 +0930

    make chrin infinte loop until properly implemented

--- c64/kernal/chrin.s
+++ c64/kernal/chrin.s
@@ -4,6 +4,10 @@
 	;; If from keyboard, then it gets a whole line of input, and returns the first char.
 	;; Repeated calls after that read out the successive bytes of the line of input.
 chrin:
+	;; XXX - Not yet implemented, so infinite loop for now
+	inc $d020
+	jmp chrin
+	
 	lda #$00
 	clc
 	rts

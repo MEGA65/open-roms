b09a016a96b5420b1eaab967bb95d136d2d01310
Paul Gardner-Stephen <paul@servalproject.org>
2019-04-19 11:41:40 +0930

    set $00/$01 values in ioinit

--- c64/kernal/ioinit.s
+++ c64/kernal/ioinit.s
@@ -1,3 +1,12 @@
 ; Function defined on pp272-273 of C64 Programmers Reference Guide
 ioinit:
+
+	; Set $00/$01 port mode and value
+	; We want BASIC and KERNAL ROMs mapped, and datasette motor off
+	; (https://www.c64-wiki.com/wiki/Zeropage)
+	LDA #$2F
+	STA $00
+	LDA #$27
+	STA $01
+
 	rts

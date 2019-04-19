ce30caeb741f9778b034026afb96354cbf6e2533
Paul Gardner-Stephen <paul@servalproject.org>
2019-04-20 02:30:56 +0930

    add missing item in keyboard matrix

--- c64/kernal/keyboard_matrix.s
+++ c64/kernal/keyboard_matrix.s
@@ -7,7 +7,7 @@ keyboard_matrixes:
 	;; Unshifted matrix built by refering to
 	;; Compute's Mapping the 64 p38-39
 	;; and C64 PRG pages 379-381
-	.byte 20,13,29,136,133,134,15
+	.byte 20,13,29,136,133,134,15,17
 	.byte $33,87,$41,$34,90,83,$45,$00
 	.byte $35,$52,$44,$36,$43,$46,$54,88
 	.byte $37,89,$47,$38,$42,$48,$55,$56

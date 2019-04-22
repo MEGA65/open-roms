03550ab786696e937a7965e76ef352fa29bac466
Paul Gardner-Stephen <paul@servalproject.org>
2019-04-23 00:07:16 +0930

    implement GETIN function

--- c64/kernal/getin.s
+++ c64/kernal/getin.s
@@ -1,3 +1,22 @@
 ; Function defined on pp272-273 of C64 Programmers Reference Guide
 getin:
+
+	;; Wait for a key
+	lda keys_in_key_buffer
+	bne +
+
+	;; Nothing in keyboard buffer to read
+	lda #$00
+	sec
+	rts
+	
+*
+	lda keyboard_buffer
+	pha
+
+	jsr pop_keyboard_buffer
+
+	pla
+	clc
+
 	rts

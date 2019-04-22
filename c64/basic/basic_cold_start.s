7f2f52052dc78299be3e496d62c1fa3340284a20
Paul Gardner-Stephen <paul@servalproject.org>
2019-04-22 20:53:37 +0930

    implement startup message

--- c64/basic/basic_cold_start.s
+++ c64/basic/basic_cold_start.s
@@ -5,7 +5,25 @@ basic_cold_start:
 	;; Setup BASIC vectors at $0300
 	;; Initialise interpretor
 	;; Print start up message
+
+	ldy #>startup_message
+	lda #<startup_message
+	jsr print_string
+	
 	;; jump into warm start loop
 	jmp basic_warm_start
 	
 	
+startup_message:
+	;; Clear the screen
+	.byte $93
+	;; Vertical bars in different colours
+	.byte $1C,$B4
+	.byte $9E,$B5
+	.byte $1E,$A1
+	;; <RVS><WHITE>MEGA<NORMAL>BASIC
+	.byte $12,$05,$4D,$45,$47,$41,$92,$42,$41,$53,$49,$43
+	;; carriage return at end
+	.byte $0D
+	
+	.byte 0

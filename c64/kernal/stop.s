b257ac82341a78fbeb5901836e87e895a3f51e68
Paul Gardner-Stephen <paul@servalproject.org>
2019-04-22 12:09:03 +0930

    begin implementing stop routine

--- c64/kernal/stop.s
+++ c64/kernal/stop.s
@@ -1,3 +1,9 @@
 ; Function defined on pp272-273 of C64 Programmers Reference Guide
 stop:
+	;; Bit 7 of BUCKYSTATUS contains the state of the STOP key
+	;; (Compute's Mapping the 64, p27)
+
+	;; Get it into the carry flag by rotating left
+	lda BUCKYSTATUS
+	rol
 	rts

6df769320a1f053bb17fe71492a8708a87010af8
Paul Gardner-Stephen <paul@servalproject.org>
2019-04-29 22:40:24 +0930

    parse line number argument in RUN statement

--- c64/basic/cmd_run.s
+++ c64/basic/cmd_run.s
@@ -1,15 +1,19 @@
 cmd_run:
-	;; XXX - Parse starting line number
-
-	jsr basic_end_of_statement_check
-	bcc +
-
-*
 	;; Reset line pointer to first line of program.
 	lda basic_start_of_text_ptr+0
 	sta basic_current_line_ptr+0
 	lda basic_start_of_text_ptr+1
 	sta basic_current_line_ptr+1
+	
+	jsr basic_end_of_statement_check
+	bcs +
 
+	jsr basic_parse_line_number
+	jsr basic_find_line
+	bcc +
+	;; Undefined Statement Error
+	ldx #16
+	jmp do_basic_error
+*
 	;; Run it!
 	jmp basic_execute_from_current_line

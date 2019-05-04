7c89705dc69ff9f109c56c7f502a1f27ddc8b445
Paul Gardner-Stephen <paul@servalproject.org>
2019-05-04 12:02:52 +0930

    nope, that didn't help at all, but did indeed result in live-lock as feared.

--- c64/kernal/iec_turnaround_to_listen.s
+++ c64/kernal/iec_turnaround_to_listen.s
@@ -9,12 +9,6 @@ iec_turnaround_to_listen:
 	ora #$20     		; assert data (bit 5)
 	sta $dd00
 
-	;; Make sure we actually see CLK release?
-	;; This could cause the CLK assertion by the talker to be missed,
-	;; so shouldn't probably be done. But it is an attempt to debug
-	;; why we never see CLK release after the turn-around.
-	jsr iec_wait_for_clock_release
-	
 	;; Wait for clock line to be asserted by the drive
 *	lda $dd00
 	sta $0421

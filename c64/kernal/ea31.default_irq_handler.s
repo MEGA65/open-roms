8f09d4e8e196beec1888e27897e242aa2f5e840b
Paul Gardner-Stephen <paul@servalproject.org>
2019-05-05 14:58:32 +0930

    fix typo. Issue #20

--- c64/kernal/ea31.default_irq_handler.s
+++ c64/kernal/ea31.default_irq_handler.s
@@ -4,4 +4,4 @@ default_irq_handler:
 	jsr scan_keyboard
 
 	;; Acknowledge CIA interrupt and return
-	jmp clear_cia1_interrupt_flag_and_return_from_interrupt:
+	jmp clear_cia1_interrupt_flag_and_return_from_interrupt

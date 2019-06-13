e49efcce67aed0c2f18acb6424652ce1d6633134
FeralChild64 <you@example.com>
2019-06-13 17:01:42 +0200

    implemented SETLFS routine

--- c64/kernal/setlfs.s
+++ c64/kernal/setlfs.s
@@ -1,3 +1,12 @@
 ; Function defined on pp272-273 of C64 Programmers Reference Guide
+
 setlfs:
+
+	;; There are 6 sane ways to implement this routine,
+	;; I hope this one won't cause similarity to CBM code
+	
+	sta current_logical_filenum
+	sty current_secondary_address
+	stx current_device_number
+	
 	rts

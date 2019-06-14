cac4e5859e7f1344d51e728697e53c473ab9d4a4
FeralChild64 <you@example.com>
2019-06-14 21:23:02 +0200

    implemented SETNAM routine

--- c64/kernal/setnam.s
+++ c64/kernal/setnam.s
@@ -1,3 +1,12 @@
 ; Function defined on pp272-273 of C64 Programmers Reference Guide
+
 setnam:
+
+	;; There are 6 sane ways to implement this routine,
+	;; I hope this one won't cause similarity to CBM code
+
+	sta current_filename_length
+	sty current_filename_ptr + 1
+	stx current_filename_ptr + 0
+
 	rts

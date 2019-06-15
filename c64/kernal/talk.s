5d0df478b02f99f78216274f494e216496f19fd6
FeralChild64 <you@example.com>
2019-06-15 17:37:17 +0200

    implemented TALK, LISTEN, UNTLK and UNLSN routines

--- c64/kernal/talk.s
+++ c64/kernal/talk.s
@@ -1,3 +1,7 @@
 ; Function defined on pp272-273 of C64 Programmers Reference Guide
+
 talk:
-	rts
+
+	;; This trivial routine is documented in 'Compute's Mapping the Commodore 64', page 223
+	ora $40
+	jmp iec_tx_byte
--- c64/kernal/untlk.s
+++ c64/kernal/untlk.s
@@ -1,3 +1,7 @@
 ; Function defined on pp272-273 of C64 Programmers Reference Guide
+
 untlk:
-	rts
+
+	;; This trivial routine is documented in 'Compute's Mapping the Commodore 64', page 224
+	lda $5F
+	jmp iec_tx_byte

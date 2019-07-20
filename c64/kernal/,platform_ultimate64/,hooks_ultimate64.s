eb8eefff827e0e521da1e281c4c184bf9e16e9f7
FeralChild64 <you@example.com>
2019-07-20 12:48:44 +0200

    disabled the prototype U64 fastloader

--- c64/kernal/,platform_ultimate64/,hooks_ultimate64.s
+++ c64/kernal/,platform_ultimate64/,hooks_ultimate64.s
@@ -3,5 +3,7 @@
 
 
 .macro PLATFORM_HOOK_LOAD
-	jsr u64_load
+	;; XXX disabled for now - the current file opening API is not
+	;;     really suitable for easy usage from Kernal
+	;; jsr u64_load
 .macend

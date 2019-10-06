99583f19c521a84443bcb8522add5f0bb5e55edc
FeralChild64 <you@example.com>
2019-10-06 23:09:51 +0200

    ugly temporary hack to support some crunchers

--- c64/basic/cmd_sys.s
+++ c64/basic/cmd_sys.s
@@ -7,9 +7,38 @@ cmd_sys:
 	jmp do_SYNTAX_error
 	
 !:
+	//
+	// XXX Temporary ugly hack to support 'SYS PI*656'
+	//
+
+	ldy #$00
+
+cmd_sys_hack_loop:
+	lda (TXTPTR), y
+	cmp cmd_sys_hack_str, y
+	bne cmd_sys_hack_not_needed
+	lda (TXTPTR), y
+	beq cmd_sys_hack_apply
+	iny
+	bne cmd_sys_hack_loop // branch always
+
+cmd_sys_hack_apply:
+
+	lda #<2060
+	sta LINNUM+0
+	lda #>2060
+	sta LINNUM+1
+	bne cmd_sys_setup_call // always jumps
+
+cmd_sys_hack_not_needed:
+
+	//
+	// XXX End of hack
+	//
+
 	jsr basic_parse_line_number
 
-	// setup call
+cmd_sys_setup_call:
 	lda #$4C // JMP opcode
 	sta USRPOK
 	lda LINNUM+0
@@ -29,3 +58,11 @@ cmd_sys:
 	jsr USRPOK
 	
 	jmp basic_execute_statement
+
+
+	//
+	// XXX Temporary ugly hack to support 'SYS PI*656'
+	//
+
+cmd_sys_hack_str:
+	.byte $FF, $AC, $36, $35, $36, $00

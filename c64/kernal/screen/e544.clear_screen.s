da2fa7ac906ff20cf94a4c7a63b8bfeb8468e449
FeralChild64 <you@example.com>
2020-01-03 20:48:38 +0100

    size-optimized screen clearing

---
+++ c64/kernal/screen/e544.clear_screen.s
@@ -0,0 +1,30 @@
+
+//
+// Clear screen and initialise line link table, described in:
+//
+// - [CM64] Computes Mapping the Commodore 64 - pages 215-216
+//
+
+
+clear_screen:
+
+	// Clear the line link table - cheecked on original ROMs,
+	// the highest bit set means that the line is NOT a logical
+	// continuation of the previous one
+
+	lda #$80
+	ldy #24	
+!:
+	sta LDTBL,y
+	dey
+	bpl !-
+
+	// Clear all the rows
+	ldx #24
+!:
+	jsr screen_clear_line
+	dex
+	bpl !-
+
+	// Fall to cursor home routine
+	jmp cursor_home

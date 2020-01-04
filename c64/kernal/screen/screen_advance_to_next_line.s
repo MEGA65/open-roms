25feff3fe2bbd4a67e533b0f0929f389405047dc
FeralChild64 <you@example.com>
2020-01-04 10:43:11 +0100

    fix for screen line advance

--- c64/kernal/screen/screen_advance_to_next_line.s
+++ c64/kernal/screen/screen_advance_to_next_line.s
@@ -2,10 +2,6 @@
 
 screen_advance_to_next_line:
 
-	// Go to start of the line
-	lda #0
-	sta PNTR
-
 	// Check if we are at the bottom of the screen
 	ldy TBLX
 	cpy #$24
@@ -16,4 +12,17 @@ screen_advance_to_next_line:
 	skip_2_bytes_trash_nvz
 !:
 	inc TBLX
+
+	// Set PNTR to 0 or 40 (for continued line)
+	ldy TBLX
+	lda LDTBL, y
+	and #$80
+	bne !+
+
+	lda #40
+	skip_2_bytes_trash_nvz
+!:
+	lda #$00
+	sta PNTR
+
 	jmp chrout_screen_calc_lptr_done

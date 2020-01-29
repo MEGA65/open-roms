861e7eedb6f4560cc0c57d8e86a972ca207480b9
FeralChild64 <you@example.com>
2020-01-29 21:31:11 +0100

    preparation for the next math routines

---
+++ c64/basic/math/ba8c.CONUPK.jmp.s
@@ -0,0 +1,7 @@
+// #LAYOUT# STD *       #TAKE
+// #LAYOUT# *   BASIC_0 #TAKE
+// #LAYOUT# *   *       #IGNORE
+
+// Just a compatibility jump - our routine is longer than original one
+
+	jmp CONUPK

55f1a1ae8a4c9010f9431d214684506f8cb36a77
FeralChild64 <you@example.com>
2020-02-27 17:40:58 +0100

    fix for multiplication by 10

--- c64/basic/math/mul10_FAC1_mul2.s
+++ c64/basic/math/mul10_FAC1_mul2.s
@@ -8,16 +8,7 @@
 
 mul10_FAC1_mul2:
 
-	// First handle the mantissa
-
-	asl FACOV
-	rol FAC1_mantissa+3
-	rol FAC1_mantissa+2
-	rol FAC1_mantissa+1
-	rol FAC1_mantissa+0
-	bcc !+
-
-	// We need to increment the exponent
+	// Increment the exponent
 
 	inc FAC1_exponent
 	bcs_16 set_FAC1_max

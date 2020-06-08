3a885a2d1496d17b1d699407233c225d309c6829
FeralChild64 <you@example.com>
2020-06-08 17:18:29 +0200

    removed some test code

--- c64/basic/engine/alloc_temp_string.s
+++ c64/basic/engine/alloc_temp_string.s
@@ -10,8 +10,7 @@
 //
 
 
-// XXX - this is a dummy implementation, until proper string handling is implemented
-// XXX - addresses are probably wrong, debug it
+// XXX - finish the implementation once string memory handling is done
 
 
 alloc_temp_string:
@@ -22,60 +21,6 @@ alloc_temp_string:
 	cpx #$1F
 	bcs_16 do_FORMULA_TOO_COMPLEX_error
 
-	// Prepare data in index registers
+	// XXX
 
-	tay                                // store length, we will need .A in a moment
-
-	// Increment temporary descriptor stack pointers by 3
-
-	lda LASTPT
-	adc #$03                           // Carry is already clear here
-	sta LASTPT
-	adc #$03
-	sta TEMPPT
-
-	// Fill in the new entry
-
-	cmp #$1C
-	bne !+
-
-	// Fill in the address - first entry
-
-	ldx TEMPPT
-	lda STREND+0
-	sta $01, x                    // address - lo byte
-	lda STREND+1
-	sta $02, x                    // address - hi byte
-
-	jmp alloc_temp_string_space_check
-!:
-	// Fill in the adress - use previous entry
-
-	ldx LASTPT
-	lda $01, x                   // address - lo byte
-	ldx TEMPPT
-	sta $01, x
-
-	ldx LASTPT
-	lda $02, x                   // address - hi byte
-	ldx TEMPPT
-	sta $02, x
-	inc $02, x
-
-	// FALLTROUGH
-
-alloc_temp_string_space_check:
-
-	// Fill in the length of the new string
-
-	ldx TEMPPT
-	tya
-	sta $00, x                    // length
-
-	// Check if we have enough space
-
-	lda $02, x
-	cmp FRETOP+1
-	beq_16 do_OUT_OF_MEMORY_error
-
-	rts
+	jmp do_NOT_IMPLEMENTED_error

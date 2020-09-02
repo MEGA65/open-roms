25dec8848a1c99b2f1e5b18eb2f04297a7fccbd3
FeralChild64 <you@example.com>
2020-09-02 21:20:45 +0200

    correction, previous bugfix was wrong

--- src/kernal/screen_m65/mega65_chrin_keyboard.s
+++ src/kernal/screen_m65/mega65_chrin_keyboard.s
@@ -120,7 +120,7 @@ m65_chrin_keyboard_enter:
 	// Retrieve first byte which is not space
 
 	jsr m65_helper_scrlpnt_chrin
-	ldy M65__TXTCOL
+	ldy M65_COLVIEWMAX // XXX add support for windowed mode here
 	iny
 
 	// FALLTROUGH

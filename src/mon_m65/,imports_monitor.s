2b650148f1991ba9e4e477efde06a02b143f8c72
FeralChild64 <you@example.com>
2020-12-14 17:55:06 +0100

    added some basic proxies for monitor

---
+++ src/mon_m65/,imports_monitor.s
@@ -0,0 +1,8 @@
+;; #IMPORT# KERNAL_0 = KERNAL_0_combined.sym
+
+;
+; Definitions for calling MEGA65 segment KERNAL_0 routines from MON_1
+;
+
+;; #ALIAS# CHRIN             = KERNAL_0.proxy_M1_CHRIN
+;; #ALIAS# CHROUT            = KERNAL_0.proxy_M1_CHROUT

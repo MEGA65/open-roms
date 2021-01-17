1d6d6df70581930cb17c27b72db4e66e757cc6f4
FeralChild64 <you@example.com>
2021-01-17 11:18:05 +0100

    initial adaptation of register set

---
+++ src/mon_m65/mon_command_set_register.s
@@ -0,0 +1,60 @@
+
+; Based on BSM (Bit Shifter's Monitor)
+
+
+Mon_Set_Register:
+
+	; PC - 16 bit
+
+	jsr  @helper_get_16_bit
+
+	lda  Long_AC+0
+	sta  PCL
+	lda  Long_AC+1
+	sta  PCH
+
+	; SR / AC / XR / YR / ZR / BP - 8 bit each
+
+	ldy  #$00
+
+@lpreg:
+
+	jsr  Get_Val_To_LAC
+	beq  @exit
+	jsr  Syntax_Byte_AC
+
+	lda  Long_AC+0
+	sta  SR,Y
+
+	iny
+	cpy  #$06
+	bne  @lpreg
+
+	; SP - 16 bit
+
+	jsr  @helper_get_16_bit
+
+	lda  Long_AC+0
+	sta  SPL
+	lda  Long_AC+1
+	sta  SPH
+
+	; Flags
+
+	; XXX
+
+@exit:
+
+	jmp Main
+
+
+@helper_get_16_bit:
+
+	lda  #$00
+	sta  Addr_Mode
+	jsr  Get_Val_To_LAC
+	beq  @exit
+	lda  Addr_Mode
+	+bne Mon_Error           ; only 16-bit value allowed
+
+	rts

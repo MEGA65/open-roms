ee736cfcbcc4beb352456c7a342bb29e4b0e46fc
Rikard Nordgren <hrn@posteo.net>
2025-01-25 18:14:31 +0100

    Add int to float conversions

--- src/basic/math/b3a2.convert_Y_to_FAC1.s
+++ src/basic/math/b3a2.convert_Y_to_FAC1.s
@@ -4,13 +4,21 @@
 ;; #LAYOUT# *   *       #IGNORE
 
 ;
-; Math package - XXX
+; Math package - convert uint8 in Y to FAC1
+;
+; Input: Y - uint8 to convert to float
+; Result: FAC1
+;
+; Stores Y into first FAC1 mantissa byte, clears the second and calls convert_i16_to_FAC1
 ;
 ; - https://www.c64-wiki.com/wiki/BASIC-ROM
 ; - https://sta.c64.org/cbm64basconv.html
 ; - https://sourceforge.net/p/acme-crossass/code-0/38/tree/trunk/ACME_Lib/cbm/c64/float.a?force=True
 ; - https://codebase64.org/doku.php?id=base:kernal_floating_point_mathematics
 ;
-; XXX add name, label, documentation, implement, test
 
-	+STUB_IMPLEMENTATION
+convert_Y_to_FAC1:
+    lda #$00
+    sta FAC1_mantissa+1
+    sty FAC1_mantissa
+    jmp convert_i16_to_FAC1
---
+++ src/basic/math/convert_i16_expsign_to_FAC1.s
@@ -0,0 +1,24 @@
+;; #LAYOUT# STD *       #TAKE
+;; #LAYOUT# *   BASIC_0 #TAKE
+;; #LAYOUT# *   *       #IGNORE
+
+;
+; Math package - convert i16 with exponent in X and sign in C to FAC1
+
+convert_i16_expsign_to_FAC1:
+
+    lda #$00                    
+    sta FAC1_mantissa+2         ; Clear high bytes of mantissa
+    sta FAC1_mantissa+3
+    stx FAC1_exponent
+    sta FAC1_sign
+    bcs @1
+
+    ; Handle signed int
+    lda FAC1_mantissa+1          ; Clear sign bit of int 16
+    and #$7F
+    sta FAC1_mantissa+1
+    ldy #>CONST_NEG_32768       ; Subtract with 32768 to invert negative number
+    lda #<CONST_NEG_32768
+    jsr add_MEM_FAC1
+@1: jmp normal_FAC1

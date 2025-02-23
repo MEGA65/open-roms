bda43d1b71efc3a4f51dd974baaae490eebce933
Rikard Nordgren <hrn@posteo.net>
2025-02-23 20:42:25 +0100

    Add COS

--- src/basic/math/e264.cos_FAC1.s
+++ src/basic/math/e264.cos_FAC1.s
@@ -5,14 +5,17 @@
 ;
 ; Math package - cosine of FAC1 in radians
 ;
+; Adds pi/2 to FAC1 and falls through to the sine function
+;
 ; See also:
 ; - [CM64] Computes Mapping the Commodore 64 - page 210
 ; - https://www.c64-wiki.com/wiki/Floating_point_arithmetic
 ; - https://codebase64.org/doku.php?id=base:kernal_floating_point_mathematics
 ;
 
-; XXX provide implementation
-
 COS:
-
-	+STUB_IMPLEMENTATION
+    ldy #>const_HALF_PI
+    lda #<const_HALF_PI
+    jsr add_MEM_FAC1
+    
+    ; FALLTHROUGH to SIN

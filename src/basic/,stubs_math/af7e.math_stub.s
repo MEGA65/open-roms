2c43a67bb0a21645272a735f15f7d679615512ff
Rikard Nordgren <hrn@posteo.net>
2025-03-05 18:02:39 +0100

    Add set_FAC1_mantissa

--- src/basic/,stubs_math/af7e.math_stub.s
+++ src/basic/,stubs_math/af7e.math_stub.s
@@ -4,7 +4,7 @@
 ;; #LAYOUT# *   *       #IGNORE
 
 ;
-; Math package - sets mantissa to 00yyxxaa
+; Math package - set rest of FAC1 and JMP to $b8d2
 ;
 ; See also:
 ; - https://codebase64.org/doku.php?id=base:kernal_floating_point_mathematics
--- src/basic/math/af87.set_FAC1_mantissa.s
+++ src/basic/math/af87.set_FAC1_mantissa.s
@@ -4,12 +4,17 @@
 ;; #LAYOUT# *   *       #IGNORE
 
 ;
-; Math package - set rest of FAC1 and JMP to $b8d2
+; Math package - sets mantissa to 00yyxxaa
 ;
 ; See also:
 ; - https://codebase64.org/doku.php?id=base:kernal_floating_point_mathematics
 ;
 
-; XXX add name, label, implement, test
 
-	+STUB_IMPLEMENTATION
+set_FAC1_mantissa:
+    sty FAC1_mantissa+1
+    stx FAC1_mantissa+2
+    sta FAC1_mantissa+3
+    lda #0
+    sta FAC1_mantissa
+    rts

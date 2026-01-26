0c10f6d3b04b6e28af069026ed0490591cb6d50c
Rikard Nordgren <hrn@posteo.net>
2026-01-26 18:40:32 +0100

    Update add_HALF_FAC1

--- src/basic/math/b849.add_HALF_FAC1.s
+++ src/basic/math/b849.add_HALF_FAC1.s
@@ -3,15 +3,19 @@
 ;; #LAYOUT# *   BASIC_0 #TAKE
 ;; #LAYOUT# *   *       #IGNORE
 
-;
+; This file is under the MIT license, it contains code released by Microsoft Corporation.
+; See LICENSE for more information.
+
 ; Math package - add 0.5 to FAC1
+;
+; This is verified to be identical to the original Microsoft implementation where it was named FADDH.
 ;
 ; See also:
 ; - [CM64] Computes Mapping the Commodore 64 - page 112
 
 add_HALF_FAC1:
 
-	ldy #>const_HALF
 	lda #<const_HALF
+	ldy #>const_HALF
 
 	jmp add_MEM_FAC1

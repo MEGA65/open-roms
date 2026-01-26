d35e3011030538db2b48850c811076a0da934149
Rikard Nordgren <hrn@posteo.net>
2026-01-26 19:16:00 +0100

    Update mov_r_FAC1_VAR

--- src/basic/math_mov/bbd0.mov_r_FAC1_VAR.s
+++ src/basic/math_mov/bbd0.mov_r_FAC1_VAR.s
@@ -3,8 +3,13 @@
 ;; #LAYOUT# *   BASIC_0 #TAKE
 ;; #LAYOUT# *   *       #IGNORE
 
-;
+
+; This file is under the MIT license, it contains code released by Microsoft Corporation.
+; See LICENSE for more information.
+
 ; Math package - move FAC1 (rounds) to memory variable
+;
+; This is verified to be identical to the original Microsoft implementation where it was named MOVVF.
 ;
 ; Input:
 ; - FORPNT ($49/$4A) - variable address
@@ -15,7 +20,7 @@
 
 mov_r_FAC1_VAR:
 
-	ldy #FORPNT+1
 	ldx #FORPNT+0
+	ldy #FORPNT+1
 
 	; FALLTROUGH to mov_r_FAC1_MEM

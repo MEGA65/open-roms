74e68f6a0d37cae9082e9bc9f6f409aac5d50092
Rikard Nordgren <hrn@posteo.net>
2026-01-31 15:05:29 +0100

    Update STATUS and similarity.c for INT

--- tools/similarity.c
+++ tools/similarity.c
@@ -20,7 +20,7 @@ int matches[MAX_SIZE];
 
 int phase_mask[MAX_SIZE+MAX_SIZE];
 
-#define NUM_CHUNKS 8
+#define NUM_CHUNKS 7
 // Pairs of start, end memory addresses of known exact similarity with the open source Microsoft BASIC
 unsigned short microsoft_basic_chunks[NUM_CHUNKS * 2] = {
     0xB849, 0xB84F,
@@ -29,8 +29,7 @@ unsigned short microsoft_basic_chunks[NUM_CHUNKS * 2] = {
     0xB983, 0xB9BB,
     0xBBA2, 0xBC1A,
     0xBC58, 0xBC5A,
-    0xBC9B, 0xBCCB,
-    0xBCE9, 0xBCF2,
+    0xBC9B, 0xBCF2,
 };
 
 

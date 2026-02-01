484e53f501c878ec05c3ac148beb9cdef202fe45
Rikard Nordgren <hrn@posteo.net>
2026-02-01 11:42:24 +0100

    Update convert_A_to_FAC1

--- src/basic/math/bc3c.convert_A_to_FAC1.s
+++ src/basic/math/bc3c.convert_A_to_FAC1.s
@@ -3,11 +3,15 @@
 ;; #LAYOUT# *   BASIC_0 #TAKE
 ;; #LAYOUT# *   *       #IGNORE
 
-;
+; This file is under the MIT license, it contains code released by Microsoft Corporation.
+; See LICENSE for more information.
+
 ; Math package - convert int8 in A to FAC1
 ;
 ; Input: A - int8 to convert to float
 ; Result: FAC1
+;
+; This is verified to be identical to the original Microsoft implementation where it was named FLOAT.
 ;
 ; Sets first byte of FAC1 to A, sign extends A into tge second byte and falls through to convert_i16_to_FAC1
 ;
@@ -15,10 +19,10 @@
 ; - https://codebase64.org/doku.php?id=base:kernal_floating_point_mathematics
 ;
 
-
 convert_A_to_FAC1:
+    sta FAC1_mantissa+0
+    lda #0
     sta FAC1_mantissa+1
-    jsr sign_extend_A_to_Y
-    sty FAC1_mantissa
-    +nop                    ; To not leave a gap to the fallthrough
+    ldx #$88
+
     ; Fall through to convert_i16_to_FAC1

061440c7e1841670143a6b810ee166999e119d1e
Rikard Nordgren <hrn@posteo.net>
2026-02-01 20:48:37 +0100

    Update mov_MEM_FAC2 and get_FAC2_via_INDEX

--- src/basic/math_mov/ba90.get_FAC2_via_INDEX.s
+++ src/basic/math_mov/ba90.get_FAC2_via_INDEX.s
@@ -3,8 +3,12 @@
 ;; #LAYOUT# *   BASIC_0 #TAKE
 ;; #LAYOUT# *   *       #IGNORE
 
-;
+; This file is under the MIT license, it contains code released by Microsoft Corporation.
+; See LICENSE for more information.
+
 ; Math package - Mov MEM to FAC2 with address in INDEX zero page register
+;
+; This is verified to be identical to the original Microsoft implementation where it was named CONUPK (partial).
 ;
 ; See also:
 ; - https://sourceforge.net/p/acme-crossass/code-0/38/tree/trunk/ACME_Lib/cbm/c64/float.a?force=True
@@ -14,36 +18,25 @@
 
 get_FAC2_via_INDEX:
 
-	; Now copy the data from RAM to FAC2, for the format description see:
-	; - https://www.c64-wiki.com/wiki/Floating_point_arithmetic#Representation_in_the_C-64
-
-	; Copy the mantissa
-
-	ldy #$04
-	lda (INDEX), y
-	sta FAC2_mantissa+3
-
-	dey
-	lda (INDEX), y
-	sta FAC2_mantissa+2
-
-	dey
-	lda (INDEX), y
-	sta FAC2_mantissa+1
-
-	dey
-	lda (INDEX), y
-	sta FAC2_sign
-	ora #$80                           ; clear the sign bit
-	sta FAC2_mantissa+0
-
-	; Copy the exponent
-
-	dey
-	lda (INDEX), y
-	sta FAC2_exponent
-
-	; Return the FAC1 exponent
-
-	lda FAC1_exponent
-	rts
+    ldy #4
+    lda (INDEX),Y
+    sta FAC2_mantissa+3
+    dey
+    lda (INDEX),Y
+    sta FAC2_mantissa+2
+    dey
+    lda (INDEX),Y
+    sta FAC2_mantissa+1
+    dey
+    lda (INDEX),Y
+    sta FAC2_sign
+    eor FAC1_sign           ; Set sign for multiplication/division
+    sta ARISGN
+    lda FAC2_sign
+    ora #$80                ; Set mantissa high bit
+    sta FAC2_mantissa+0
+    dey
+    lda (INDEX),Y
+    sta FAC2_exponent
+    lda FAC1_exponent       ; Set flags from FAC1_exponent
+    rts

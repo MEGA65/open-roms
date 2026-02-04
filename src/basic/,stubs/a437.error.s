9fb88c4348cffd45fc1f10716f7c538e1f8862b8
Rikard Nordgren <hrn@posteo.net>
2026-02-04 20:24:02 +0100

    Add overflow_error

---
+++ src/basic/,stubs/a437.error.s
@@ -0,0 +1,12 @@
+;; #LAYOUT# STD *       #TAKE
+;; #LAYOUT# X16 BASIC_0 #TAKE-OFFSET 2000
+;; #LAYOUT# *   BASIC_0 #TAKE
+;; #LAYOUT# *   *       #IGNORE
+
+
+
+; XXX provide documentation and implementation
+
+error:
+
+    +STUB_IMPLEMENTATION
---
+++ src/basic/math/b97e.overflow_error.s
@@ -0,0 +1,17 @@
+;; #LAYOUT# STD *       #TAKE
+;; #LAYOUT# X16 BASIC_0 #TAKE-OFFSET 2000
+;; #LAYOUT# *   BASIC_0 #TAKE
+;; #LAYOUT# *   *       #IGNORE
+
+; This file is under the MIT license, it contains code released by Microsoft Corporation.
+; See LICENSE for more information.
+
+; Math package - overflow error
+;
+; This is verified to be identical to the original Microsoft implementation where it was named OVERR.
+;
+
+overflow_error:
+
+    ldx #err_overflow
+    jmp error

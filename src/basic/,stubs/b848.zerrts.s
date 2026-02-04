ecaae14a8fdc5e90a4a6c37cf467e9b69b1bef1d
Rikard Nordgren <hrn@posteo.net>
2026-02-04 20:29:26 +0100

    Add temporary stub for zerrts

---
+++ src/basic/,stubs/b848.zerrts.s
@@ -0,0 +1,9 @@
+;; #LAYOUT# STD *       #TAKE
+;; #LAYOUT# X16 BASIC_0 #TAKE-OFFSET 2000
+;; #LAYOUT# *   BASIC_0 #TAKE
+;; #LAYOUT# *   *       #IGNORE
+
+
+ZERRTS:  ; FIXME: Here for the time being. Should be moved to FNWAIT
+
+    rts

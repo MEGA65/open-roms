fe3853274892796f2f51899601a03e4ab855f695
Rikard Nordgren <hrn@posteo.net>
2026-02-04 20:24:27 +0100

    Remove old normal_FAC1 compatibility jump

--- src/basic/math/b8d7.normal_FAC1.jmp.s
+++
@@ -1,9 +0,0 @@
-;; #LAYOUT# STD *       #TAKE
-;; #LAYOUT# X16 BASIC_0 #TAKE-OFFSET 2000
-;; #LAYOUT# *   BASIC_0 #TAKE
-;; #LAYOUT# *   *       #IGNORE
-
-; Just a compatibility jump - our routine is longer than original one
-
-normal_FAC1:
-	jmp normal_FAC1_impl

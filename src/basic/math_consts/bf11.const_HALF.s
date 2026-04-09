d01bddd7b50f1180b6eefe91dcb8157c2558b26f
Rikard Nordgren <hrn@posteo.net>
2026-04-09 21:15:21 +0200

    Update const_HALF

--- src/basic/math_consts/bf11.const_HALF.s
+++ src/basic/math_consts/bf11.const_HALF.s
@@ -3,8 +3,13 @@
 ;; #LAYOUT# *   BASIC_0 #TAKE
 ;; #LAYOUT# *   *       #IGNORE
 
+; This file is under the MIT license, it contains code released by Microsoft Corporation.
+; See LICENSE for more information.
+
+; Math package - the constant 1/2
 ;
-; Math package - a constant, for documentation see:
+; This is verified to be identical to the original Microsoft implementation where it was named FHALF
+;
 ; - https://www.c64-wiki.com/wiki/BASIC-ROM
 ; - Computes Mapping the Commodore 64, page 116
 ;

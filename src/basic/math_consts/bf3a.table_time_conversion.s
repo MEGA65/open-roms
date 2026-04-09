b10f51345ae191c2970d53be21f9c1179704a0a6
Rikard Nordgren <hrn@posteo.net>
2026-04-09 21:41:38 +0200

    Add time conversion table

---
+++ src/basic/math_consts/bf3a.table_time_conversion.s
@@ -0,0 +1,20 @@
+;; #LAYOUT# STD *       #TAKE
+;; #LAYOUT# X16 BASIC_0 #TAKE-OFFSET 2000
+;; #LAYOUT# *   BASIC_0 #TAKE
+;; #LAYOUT# *   *       #IGNORE
+
+; This file is under the MIT license, it contains code released by Microsoft Corporation.
+; See LICENSE for more information.
+
+; Math package - time conversion table
+;
+; This is verified to be identical to the original Microsoft implementation
+
+
+    !be32 -2160000
+    !be32 216000
+    !be32 -36000
+    !be32 3600
+    !be32 -600
+    !be32 60
+TIMEND:

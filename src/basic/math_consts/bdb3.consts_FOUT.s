abd67c40f9ca3389247bfb72feb11701a190e4cf
Rikard Nordgren <hrn@posteo.net>
2026-04-10 20:32:55 +0200

    Add floating point constants for FOUT

---
+++ src/basic/math_consts/bdb3.consts_FOUT.s
@@ -0,0 +1,24 @@
+;; #LAYOUT# STD *       #TAKE
+;; #LAYOUT# X16 BASIC_0 #TAKE-OFFSET 2000
+;; #LAYOUT# *   BASIC_0 #TAKE
+;; #LAYOUT# *   *       #IGNORE
+
+; This file is under the MIT license, it contains code released by Microsoft Corporation.
+; See LICENSE for more information.
+
+; Math package - constants used by FOUT
+;
+; This is verified to be identical to the original Microsoft implementation
+;
+
+const_NZ0999:
+
+	+PUT_CONST_NZ0999
+	
+const_NZ9999:
+
+	+PUT_CONST_NZ9999
+
+const_NZMIL:
+
+	+PUT_CONST_NZMIL

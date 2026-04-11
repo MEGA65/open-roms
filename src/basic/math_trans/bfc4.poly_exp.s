fc1e7fd393198057ee5c9bc678556eaa75bdb0a3
Rikard Nordgren <hrn@posteo.net>
2026-04-11 18:05:25 +0200

    Update poly_exp

--- src/basic/math_trans/bfc4.poly_exp.s
+++ src/basic/math_trans/bfc4.poly_exp.s
@@ -3,7 +3,13 @@
 ;; #LAYOUT# *  BASIC_0  #TAKE
 ;; #LAYOUT# *   *       #IGNORE
 
+
+; This file is under the MIT license, it contains code released by Microsoft Corporation.
+; See LICENSE for more information.
+
 ; Polynomial for the exponential function
+
+; This is verified to be identical to the original Microsoft implementation where it was named EXPCON.
 
 
 !ifdef CONFIG_TRANSCENDENTAL_FUNCTIONS {
@@ -20,7 +26,7 @@ poly_exp:
     +PUT_CONST_POLY_EXP_5
     +PUT_CONST_POLY_EXP_6
     +PUT_CONST_POLY_EXP_7
-    +PUT_CONST_POLY_EXP_8
 
+    ; The constant const_ONE now follows as part of this polynomial
 
 }

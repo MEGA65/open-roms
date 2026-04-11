43ea6a4cac5a4bbc0266ab4cb007ab835be4ceda
Rikard Nordgren <hrn@posteo.net>
2026-04-11 17:18:55 +0200

    Update const_INV_LOG_2

--- src/basic/math_consts/bfbf.const_INV_LOG_2.s
+++ src/basic/math_consts/bfbf.const_INV_LOG_2.s
@@ -3,8 +3,14 @@
 ;; #LAYOUT# *   BASIC_0 #TAKE
 ;; #LAYOUT# *   *       #IGNORE
 
+; This file is under the MIT license, it contains code released by Microsoft Corporation.
+; See LICENSE for more information.
+
+; Math package - log2(e) = 1 / log(2)
 ;
-; Math package - a constant, for documentation see:
+; This is verified to be identical to the original Microsoft implementation where it was named LOGEB2
+;
+; See also:
 ; - https://www.c64-wiki.com/wiki/BASIC-ROM
 ;
 

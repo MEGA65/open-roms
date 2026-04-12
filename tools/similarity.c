866d1ffb3587a0f8a4fa925e5a8ba06a51bb2859
Rikard Nordgren <hrn@posteo.net>
2026-04-12 21:26:04 +0200

    Update const_ONE

--- tools/similarity.c
+++ tools/similarity.c
@@ -20,14 +20,13 @@ int matches[MAX_SIZE];
 
 int phase_mask[MAX_SIZE+MAX_SIZE];
 
-#define NUM_CHUNKS 10
+#define NUM_CHUNKS 9
 // Pairs of start, end memory addresses of known exact similarity with the open source Microsoft BASIC
 unsigned short microsoft_basic_chunks[NUM_CHUNKS * 2] = {
     0xB391, 0xB39D,
     0xB3A2, 0xB3A5,
     0xB849, 0xB97D,
-    0xB983, 0xB9BB,
-    0xB9C1, 0xB9D5,
+    0xB983, 0xB9D5,
     0xBA28, 0xBCF2,
     0xBD7E, 0xBD90,
     0xBDB3, 0xBDC1,

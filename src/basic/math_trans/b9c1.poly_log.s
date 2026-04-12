ea434f9e389c3a372942bb40cfcc74fe52212f28
Rikard Nordgren <hrn@posteo.net>
2026-04-12 21:23:05 +0200

    Update poly_log

--- src/basic/math_trans/b9c1.poly_log.s
+++ src/basic/math_trans/b9c1.poly_log.s
@@ -3,8 +3,13 @@
 ;; #LAYOUT# *  BASIC_0  #TAKE
 ;; #LAYOUT# *   *       #IGNORE
 
+
+; This file is under the MIT license, it contains code released by Microsoft Corporation.
+; See LICENSE for more information.
+
 ; Polynomial for the logarithm function
 
+; This is verified to be identical to the original Microsoft implementation where it was named LOGCN2
 
 !ifdef CONFIG_TRANSCENDENTAL_FUNCTIONS {
 

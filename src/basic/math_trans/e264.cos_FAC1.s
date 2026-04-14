3927fdacac02d0e5fbce7141e8172e299f3d4f0a
Rikard Nordgren <hrn@posteo.net>
2026-04-14 19:28:50 +0200

    Update cos_FAC1

--- src/basic/math_trans/e264.cos_FAC1.s
+++ src/basic/math_trans/e264.cos_FAC1.s
@@ -2,8 +2,12 @@
 ;; #LAYOUT# *   BASIC_0 #TAKE
 ;; #LAYOUT# *   *       #IGNORE
 
-;
+; This file is under the MIT license, it contains code released by Microsoft Corporation.
+; See LICENSE for more information.
+
 ; Math package - cosine of FAC1 in radians
+;
+; This is verified to be identical to the original Microsoft implementation where it was named COS
 ;
 ; Adds pi/2 to FAC1 and falls through to the sine function
 ;
@@ -18,10 +22,11 @@
 
 
 COS:
-    ldy #>const_HALF_PI
+
     lda #<const_HALF_PI
+    ldy #>const_HALF_PI
     jsr add_MEM_FAC1
-    
+ 
     ; FALLTHROUGH to SIN
 
 

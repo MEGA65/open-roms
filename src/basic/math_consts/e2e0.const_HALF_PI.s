a07bce566bd9dcdd72b52a82b92fde5cc1c0ebe2
Rikard Nordgren <hrn@posteo.net>
2026-04-14 20:34:00 +0200

    Update const_HALF_PI

--- src/basic/math_consts/e2e0.const_HALF_PI.s
+++ src/basic/math_consts/e2e0.const_HALF_PI.s
@@ -2,8 +2,13 @@
 ;; #LAYOUT# *   BASIC_0 #TAKE
 ;; #LAYOUT# *   *       #IGNORE
 
-;
+; This file is under the MIT license, it contains code released by Microsoft Corporation.
+; See LICENSE for more information.
+
 ; Math package - a constant, for documentation see:
+;
+; This is verified to be identical to the original Microsoft implementation where it was named PI2.
+;
 ; - https://sourceforge.net/p/acme-crossass/code-0/38/tree/trunk/ACME_Lib/cbm/c64/float.a?force=True
 ;
 

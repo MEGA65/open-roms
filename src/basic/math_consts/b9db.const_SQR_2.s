70bf9c6a81ccbc3fdf70551adcfd34691e2ab6a1
Rikard Nordgren <hrn@posteo.net>
2026-04-15 20:40:58 +0200

    Update const_SQR_2

--- src/basic/math_consts/b9db.const_SQR_2.s
+++ src/basic/math_consts/b9db.const_SQR_2.s
@@ -3,8 +3,13 @@
 ;; #LAYOUT# *   BASIC_0 #TAKE
 ;; #LAYOUT# *   *       #IGNORE
 
+; This file is under the MIT license, it contains code released by Microsoft Corporation.
+; See LICENSE for more information.
+
+; Math package - SQR(2)
 ;
-; Math package - a constant, for documentation see:
+; This is verified to be identical to the original Microsoft implementation where it was named SQRTWO.
+;
 ; - https://www.c64-wiki.com/wiki/BASIC-ROM
 ;
 
